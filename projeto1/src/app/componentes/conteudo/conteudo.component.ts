ee5c93110623a2bf40f078e0de0b5e5f700b273f
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-07-06 17:14:34 -0300

    Episódio 05 - Ciclo de Vida, interpolação, event e property binding

--- projeto1/src/app/componentes/conteudo/conteudo.component.ts
+++ projeto1/src/app/componentes/conteudo/conteudo.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-conteudo',
   templateUrl: './conteudo.component.html',
   styleUrl: './conteudo.component.css'
 })
-export class ConteudoComponent {
+export class ConteudoComponent implements OnInit {
 
+  public mensagem: string;
+  public valor: number;
+  
+  public constructor() {
+    this.mensagem = "Olá Mundo!";
+    this.valor = 0;
+  }
+  
+  ngOnInit(): void {
+      
+  }
+
+  public mudarValor() {
+    this.valor++;
+    // console.log(this.valor);
+  }
 }
